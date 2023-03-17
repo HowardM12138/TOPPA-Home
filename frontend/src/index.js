3d66885c2f8ea9e0cab2c8a0a93a4a9ef2065a29
coccooma <coccoo.ma@berkeley.edu>
2023-03-16 18:56:58 -0700

    tab space all

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,30 +14,30 @@ import BondingGroup from "./components/BondingGroup";
 import "./index.css";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <PortalHomePage />,
-        errorElement: <PortalHomePage />,
-    },
-    {
-        path: "sign-in",
-        element: <SignIn />,
-    },
-    {
-        path: "Reimbursement",
-        element: <Reimbursement />,
-    },
-    {
-        path: "BondingGroupBoard",
-        element: <BondingGroup />,
-    },
+  {
+    path: "/",
+    element: <PortalHomePage />,
+    errorElement: <PortalHomePage />,
+  },
+  {
+    path: "sign-in",
+    element: <SignIn />,
+  },
+  {
+    path: "Reimbursement",
+    element: <Reimbursement />,
+  },
+  {
+    path: "BondingGroupBoard",
+    element: <BondingGroup />,
+  },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <GoogleOAuthProvider clientId="825257094322-i2kbt4aa3i3tp3a3cmfdh41uqc8b69s7.apps.googleusercontent.com">
-        <React.StrictMode>
-            <RouterProvider router={router} />
-        </React.StrictMode>
-    </GoogleOAuthProvider>
+  <GoogleOAuthProvider clientId="825257094322-i2kbt4aa3i3tp3a3cmfdh41uqc8b69s7.apps.googleusercontent.com">
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </GoogleOAuthProvider>
 );
